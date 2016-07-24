10d133f316fe2a4c8c991333a8c25b2707a40252
se <sijun@hotmail.fr>
2016-07-24 16:19:35 +0200

    explication

--- src/game/chesspanel.java
+++ src/game/chesspanel.java
@@ -8,11 +8,16 @@ import javax.swing.JFrame;
 
 public class chesspanel extends JFrame implements ActionListener {
 	
+	//电脑屏幕
 	int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     int height = Toolkit.getDefaultToolkit().getScreenSize().height;
 	
      public void chessframe(){
+    	 
+    	 //游戏名字
     	 setTitle("puissance 5");
+    	 
+    	 //窗口大小不可变，窗口大小设置，窗口出现位置
     	 setResizable(false);
     	 setSize(500,500);
     	 setLocation((width-500)/2,(height-500)/2);
